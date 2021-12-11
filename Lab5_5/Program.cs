68f2d365ca4525a4b60b7a76ea2aba1e32e0a979
Антон Еремеев <kzanton_81@mail.ru>
2021-12-12 00:55:53 +0300

    Добавьте файлы проекта.

---
+++ Lab5_5/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Запросить у пользователя целочисленное значение N. Сформировать двумерный массив размера NxN следующего вида:
+1 0 1 0 1
+0 1 0 1 0
+1 0 1 0 1
+0 1 0 1 0
+1 0 1 0 1
+");
+            Console.WriteLine("");
+
+            Console.WriteLine("Введите размерность массива:");
+            var dim = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Результат :");
+            Console.WriteLine("");
+
+            int[,] array = new int[dim, dim];
+
+            
+            for (int i = 0; i < dim; i++)
+            {
+                for (int j = 0; j < dim; j++)
+                {
+                    
+                    array[i, j] = i % 2 != 0 ? (j % 2 != 0 ? 1 : 0) : (j % 2 != 0 ? 0 : 1);
+
+                    Console.Write(array[i, j] + " ");
+                }
+
+               
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
